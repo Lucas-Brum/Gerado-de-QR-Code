59a40898adfbeeffdd30f911e0029bf94bde5eab
Lucas Matheus Rodrigues Brum <lucasmatheusrodriguesbrum@gmail.com>
2021-03-03 22:39:49 -0300

    add arquivos

---
+++ index.js
@@ -0,0 +1,8 @@
+function GeraQRCode()		
+        {
+        var conteudo = document.getElementById('conteudoQRCode').value;
+        var GoogleCharts = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=';
+        var imagemQRCode = GoogleCharts + conteudo;
+        document.getElementById('imageQRCode').src = imagemQRCode;
+        
+        }
