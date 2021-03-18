ccaeb04e161dd967cc7fbfbd911ff12fbd80f4ec
Lucas-Brum <lucasmatheusrodriguesbrum@gmail.com>
2021-03-17 22:50:05 -0300

    Trava do botão

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+
 function GeraQRCode()		
         {
-        var conteudo = document.getElementById('conteudoQRCode').value;
-        var GoogleCharts = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=';
-        var imagemQRCode = GoogleCharts + conteudo;
-        document.getElementById('imageQRCode').src = imagemQRCode;
+
+                var conteudo = document.getElementById('conteudoQRCode').value;
+                var GoogleCharts = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=';
+                var imagemQRCode = GoogleCharts + conteudo;
+
+                if (conteudo == 'Conteudo do QR Code' || conteudo == '') {
+                        console.log('invalido')
+                        alert("Digite sua URL na caixa de texto");
+                      } 
+                else {  
+                        console.log('valido')
+                        document.getElementById('imageQRCode').src = imagemQRCode;
+                      }
+
+                
         
         }
