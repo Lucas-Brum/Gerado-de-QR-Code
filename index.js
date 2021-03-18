2916049fc06ed87181a7eeb3db3982dd1e85f84f
Lucas-Brum <lucasmatheusrodriguesbrum@gmail.com>
2021-03-17 23:17:26 -0300

    Correção trava do botão

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 function GeraQRCode()		
         {
 
+                var regex = new RegExp("^((http(s?)://(www.)?[a-z]+.com/)|(magnet:?xt=urn:btih:))")
                 var conteudo = document.getElementById('conteudoQRCode').value;
                 var GoogleCharts = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=';
                 var imagemQRCode = GoogleCharts + conteudo;
 
-                if (conteudo == 'Conteudo do QR Code' || conteudo == '') {
-                        console.log('invalido')
-                        alert("Digite sua URL na caixa de texto");
-                      } 
-                else {  
-                        console.log('valido')
+                if (regex.test(conteudo)) {
                         document.getElementById('imageQRCode').src = imagemQRCode;
-                      }
+                        
+                } 
+                else {  
+                        alert("Digite sua URL na caixa de texto");
+                }
 
                 
         
